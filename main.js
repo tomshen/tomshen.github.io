239beee16be02bb6588c69841ec60cad386f4d24
Tom Shen <tom@shen.io>
2016-12-15 10:19:48 +1100

    Add current location

---
+++ main.js
@@ -0,0 +1,18 @@
+fetch("http://wrzt.tomshen.me/")
+.then(res => res.json())
+.then(data => {
+    if (data) {
+        let city = data.city;
+        let country = data.country;
+        
+        if (city && country) {
+            let section = document.getElementsByTagName("section")[0];
+            let newParagraph = document.createElement("p");
+            let newParagraphContent = document.createTextNode("I am currently in "
+                + city + ", " + country + ".");
+
+            newParagraph.appendChild(newParagraphContent);
+            section.appendChild(newParagraph);  
+        }
+    }
+});
